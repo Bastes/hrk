0d6789eb29929062087f6c7f5d98d51bd9d207f8
Michel Belleville <mib@octo.com>
2015-02-10 16:21:23 +0100

    First step towards simplifying the command executer

--- lib/hrk/execute/command.rb
+++ lib/hrk/execute/command.rb
@@ -10,26 +10,23 @@ module Hrk
       end
 
       def call *args
-        remote, command = remote_and_command args
+        command, remote = command_and_remote args
         if remote
+          raise ArgumentError.new "Invalid remote option" if remote.length != 2
           @env.remote = remote
           Hrk::Heroku.new(*remote).call(*command)
         else
-          raise ArgumentError.new("No remote has been previously defined and the command does not explicitely mention a remote") unless @env.remote?
+          raise ArgumentError.new "No remote has been previously defined and the command does not explicitely mention a remote" unless @env.remote?
           Hrk::Heroku.new(*@env.remote).call(*command)
         end
       end
 
-      def remote_and_command args
-        args.slice_before { |arg| arg =~ ARG }.inject([nil, []]) do |r, slice|
-          if slice.first =~ ARG
-            raise ArgumentError.new('Remote option without value') if slice.length < 2
-            raise ArgumentError.new('Too many remotes mentionned') if r.first
-            [slice.take(2), r.last + slice.drop(2)]
-          else
-            [r.first, r.last + slice]
-          end
-        end
+      private
+
+      def command_and_remote args
+        command, remote, *other_remotes = ([nil] + args).slice_before(ARG).to_a
+        raise ArgumentError.new "Too many remotes or app" if other_remotes.any?
+        [command.compact, remote]
       end
     end
   end
--- spec/hrk/execute/command_spec.rb
+++ spec/hrk/execute/command_spec.rb
@@ -1,7 +1,7 @@
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe Hrk::Execute::Command do
-  describe '#call' do
+  describe "#call" do
     subject(:command) { Hrk::Execute::Command.new }
 
     let(:heroku) { double("Hrk::Heroku") }
@@ -10,10 +10,10 @@ RSpec.describe Hrk::Execute::Command do
 
     before { allow(command.env).to receive(:remote=) }
 
-    context 'no remote was previously memorized' do
+    context "no remote was previously memorized" do
       before { allow(command.env).to receive(:remote?).and_return(nil) }
 
-      describe 'standard case (a remote, a command)' do
+      describe "standard case (a remote, a command)" do
         let(:remote) { "remote-#{rand(1..9)}" }
         let(:args)   { %w(whatever that:may -b) }
 
@@ -23,12 +23,12 @@ RSpec.describe Hrk::Execute::Command do
               context "when the command returns #{result}" do
                 before { allow(heroku).to receive(:call).and_return(result) }
 
-                it { expect(command.call(opt, remote, *args)).to eq result }
+                it { expect(command.call(*args, opt, remote)).to eq result }
               end
             end
 
-            describe 'interactions' do
-              before { command.call(opt, remote, *args) }
+            describe "interactions" do
+              before { command.call(*args, opt, remote) }
 
               it { expect(Hrk::Heroku).to have_received(:new).with(opt, remote) }
               it { expect(heroku).to have_received(:call).with(*%w{whatever that:may -b}) }
@@ -38,30 +38,55 @@ RSpec.describe Hrk::Execute::Command do
         end
       end
 
-      describe 'edge cases (no remote, improper remote, too many remotes...)' do
-        let(:other_args) { %W(something-#{rand(1..9)} -o r --another) }
+      describe "edge cases" do
+        describe "a remote, nocommand" do
+          let(:remote) { "remote-#{rand(1..9)}" }
 
-        before { expect(heroku).not_to receive(:call) }
-        before { expect(command.env).not_to receive(:remote=) }
+          %w(-a -r).each do |opt|
+            context "using #{opt}" do
+              [true, false].each do |result|
+                context "when the command returns #{result}" do
+                  before { allow(heroku).to receive(:call).and_return(result) }
 
-        it { expect { command.call }.to raise_error ArgumentError }
-        it { expect { command.call "parameterless-remote", *other_args }.to raise_error ArgumentError }
-        it { expect { command.call(*other_args) }.to raise_error ArgumentError }
-        it { expect { command.call(*other_args, '-r') }.to raise_error ArgumentError }
-        it { expect { command.call(*other_args, '-a') }.to raise_error ArgumentError }
-        it { expect { command.call '-r', 'remote', *other_args, '-r', 'other-remote' }.to raise_error ArgumentError }
-        it { expect { command.call '-r', 'remote', *other_args, '-a', 'app' }.to raise_error ArgumentError }
-        it { expect { command.call '-a', 'app', *other_args, '-a', 'other-app' }.to raise_error ArgumentError }
-        it { expect { command.call '-r', 'something', *other_args, '-a', 'something-else' }.to raise_error ArgumentError }
+                  it { expect(command.call(opt, remote)).to eq result }
+                end
+              end
+
+              describe "interactions" do
+                before { command.call(opt, remote) }
+
+                it { expect(Hrk::Heroku).to have_received(:new).with(opt, remote) }
+                it { expect(heroku).to have_received(:call).with no_args }
+                it { expect(command.env).to have_received(:remote=).with([opt, remote]) }
+              end
+            end
+          end
+        end
+
+        describe "no remote, improper remote..." do
+          let(:other_args) { %W(something-#{rand(1..9)} -o r --another) }
+
+          before { expect(heroku).not_to receive(:call) }
+          before { expect(command.env).not_to receive(:remote=) }
+
+          it { expect { command.call }.to raise_error ArgumentError }
+          it { expect { command.call(*other_args) }.to raise_error ArgumentError }
+          it { expect { command.call(*other_args, "-r") }.to raise_error ArgumentError }
+          it { expect { command.call(*other_args, "-a") }.to raise_error ArgumentError }
+          it { expect { command.call(*other_args, *%w(-r demo -r prod)) }.to raise_error ArgumentError }
+          it { expect { command.call(*other_args, *%w(-a app -a other-app)) }.to raise_error ArgumentError }
+          it { expect { command.call(*other_args, *%w(-r staging -a server)) }.to raise_error ArgumentError }
+          it { expect { command.call(*other_args, *%w(-a machine -r dennis)) }.to raise_error ArgumentError }
+        end
       end
     end
 
-    context 'a remote was previously memorized' do
+    context "a remote was previously memorized" do
       let(:previous_remote) { "ye-olde-remote#{rand(1..9)}" }
-      before { allow(command.env).to receive(:remote).and_return(['-r', previous_remote]) }
+      before { allow(command.env).to receive(:remote).and_return(["-r", previous_remote]) }
       before { allow(command.env).to receive(:remote?).and_return(true) }
 
-      describe 'standard case (a remote, a command)' do
+      describe "standard case (a remote, a command)" do
         let(:remote) { "remote-#{rand(1..9)}" }
         let(:args)   { %w(whatever that:may -b) }
 
@@ -69,20 +94,20 @@ RSpec.describe Hrk::Execute::Command do
           describe "when the command returns #{result}" do
             before { allow(heroku).to receive(:call).and_return(result) }
 
-            it { expect(command.call('-r', remote, *args)).to eq result }
+            it { expect(command.call(*args, "-r", remote)).to eq result }
           end
         end
 
-        describe 'interactions' do
-          before { command.call '-r', remote, *args }
+        describe "interactions" do
+          before { command.call *args, "-r", remote }
 
-          it { expect(Hrk::Heroku).to have_received(:new).with('-r', remote) }
+          it { expect(Hrk::Heroku).to have_received(:new).with("-r", remote) }
           it { expect(heroku).to have_received(:call).with(*%w{whatever that:may -b}) }
-          it { expect(command.env).to have_received(:remote=).with(['-r', remote]) }
+          it { expect(command.env).to have_received(:remote=).with(["-r", remote]) }
         end
       end
 
-      describe 'use previous remote' do
+      describe "use previous remote" do
         let(:args)   { %w(whatever that:may -b) }
 
         [true, false].each do |result|
@@ -93,48 +118,23 @@ RSpec.describe Hrk::Execute::Command do
           end
         end
 
-        describe 'interactions' do
+        describe "interactions" do
           before { command.call(*args) }
 
-          it { expect(Hrk::Heroku).to have_received(:new).with('-r', previous_remote) }
+          it { expect(Hrk::Heroku).to have_received(:new).with("-r", previous_remote) }
           it { expect(heroku).to have_received(:call).with(*%w{whatever that:may -b}) }
         end
       end
 
-      describe 'edge cases (improper remote, too many remotes...)' do
+      describe "edge cases (improper remote)" do
         let(:other_args) { %W(--whatever#{rand(1..9)} dude) }
 
         before { expect(heroku).not_to receive(:call) }
         before { expect(command.env).not_to receive(:remote=) }
 
-        it { expect { command.call(*other_args, '-r') }.to raise_error ArgumentError }
-        it { expect { command.call(*other_args, '-a') }.to raise_error ArgumentError }
-        it { expect { command.call('-r', 'remote', *other_args, '-r', 'other-remote') }.to raise_error ArgumentError }
-        it { expect { command.call('-r', 'remote', *other_args, '-a', 'app') }.to raise_error ArgumentError }
-        it { expect { command.call('-a', 'app', *other_args, '-a', 'other-app') }.to raise_error ArgumentError }
-        it { expect { command.call('-r', 'something', *other_args, '-a', 'something-else') }.to raise_error ArgumentError }
+        it { expect { command.call(*other_args, "-r") }.to raise_error ArgumentError }
+        it { expect { command.call(*other_args, "-a") }.to raise_error ArgumentError }
       end
-    end
-  end
-
-  describe '#remote_and_command' do
-    subject(:command) { Hrk::Execute::Command.new }
-
-    context '(standard cases, arguments parsed)' do
-      it { expect(command.remote_and_command(%w(run rake db:migrate))).to eq [nil, %w(run rake db:migrate)] }
-      it { expect(command.remote_and_command(%w(restart))).to eq [nil, %w(restart)] }
-      it { expect(command.remote_and_command(%w(-r demo restart))).to eq [%w(-r demo), %w(restart)] }
-      it { expect(command.remote_and_command(%w(run rake db:migrate -r prod))).to eq [%w(-r prod), %w(run rake db:migrate)] }
-      it { expect(command.remote_and_command(%w(some command -r test -i dont know --about))).to eq [%w(-r test), %w(some command -i dont know --about)] }
-    end
-
-    context '(edge cases, argument errors)' do
-      it { expect { command.remote_and_command(%w(-r staging logs -t -r test)) }.to raise_error ArgumentError }
-      it { expect { command.remote_and_command(%w(-a prod run rake db:migrate -r echo)) }.to raise_error ArgumentError }
-      it { expect { command.remote_and_command(%w(run console -r demo -a app)) }.to raise_error ArgumentError }
-      it { expect { command.remote_and_command(%w(-a one -a other maintenance:on)) }.to raise_error ArgumentError }
-      it { expect { command.remote_and_command(%w(maintenance:off -a)) }.to raise_error ArgumentError }
-      it { expect { command.remote_and_command(%w(restart -r)) }.to raise_error ArgumentError }
     end
   end
 end
